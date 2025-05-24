e3f79c285713fd4940dd0124cabcf547c6443a42
yeeun0702 <kimyeeun0702@naver.com>
2025-05-24 19:38:44 +0900

    ✨ [feat] #12 Todo 도메인 구현

---
+++ bbangzip-domain/src/main/java/org/sopt/todo/domain/TodoTableConstants.java
@@ -0,0 +1,12 @@
+package org.sopt.todo.domain;
+
+public class TodoTableConstants {
+    public static final String TABLE_TODO = "todo";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_CATEGORY_ID = "category_id";
+    public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_START_TIME = "start_time";
+    public static final String COLUMN_IS_COMPLETED = "is_completed";
+    public static final String COLUMN_TARGET_DATE = "target_date";
+    public static final String COLUMN_ORDER = "order";
+}
