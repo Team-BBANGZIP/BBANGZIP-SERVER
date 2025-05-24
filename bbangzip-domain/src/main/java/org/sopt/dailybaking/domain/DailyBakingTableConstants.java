511a0c03da58c580af460e4422136b949bb817b9
yeeun0702 <kimyeeun0702@naver.com>
2025-05-24 19:38:20 +0900

    ✨ [feat] #12 DailyBaking 도메인 구현

---
+++ bbangzip-domain/src/main/java/org/sopt/dailybaking/domain/DailyBakingTableConstants.java
@@ -0,0 +1,9 @@
+package org.sopt.dailybaking.domain;
+
+public class DailyBakingTableConstants {
+    public static final String TABLE_DAILY_BAKING = "dailybaking";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_BAKE_DATE = "bake_date";
+    public static final String COLUMN_BREAD_COUNT = "bread_count";
+}
