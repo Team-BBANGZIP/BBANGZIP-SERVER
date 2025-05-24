a97c20a30bd5e0c9c9702807ecafd419a83eb0b9
yeeun0702 <kimyeeun0702@naver.com>
2025-05-24 19:38:55 +0900

    ✨ [feat] #12 UserBread 도메인 구현

---
+++ bbangzip-domain/src/main/java/org/sopt/userbread/domain/UserBreadTableConstants.java
@@ -0,0 +1,9 @@
+package org.sopt.userbread.domain;
+
+public class UserBreadTableConstants {
+    public static final String TABLE_USER_BREAD = "userbread";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_BREAD_ID = "bread_id";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_IS_UNLOCKED = "is_unlocked";
+}
