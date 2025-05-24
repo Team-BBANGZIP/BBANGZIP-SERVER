7039bac6597984dddc8ac9a285b6f676a7782685
yeeun0702 <kimyeeun0702@naver.com>
2025-05-24 19:38:55 +0900

    ✨ [feat] #12 UserBread 도메인 구현

---
+++ bbangzip-domain/src/main/java/org/sopt/userbread/domain/UserBreadEntity.java
@@ -0,0 +1,35 @@
+package org.sopt.userbread.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.bread.domain.BreadEntity;
+import org.sopt.common.BaseTimeEntity;
+import org.sopt.user.domain.UserEntity;
+
+import static org.sopt.userbread.domain.UserBreadTableConstants.*;
+
+@Entity
+@Getter
+@Table(name = TABLE_USER_BREAD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserBreadEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = COLUMN_ID)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = COLUMN_USER_ID, nullable = false)
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = COLUMN_BREAD_ID, nullable = false)
+    private BreadEntity bread;
+
+    @Column(name = COLUMN_IS_UNLOCKED, nullable = false)
+    private Boolean isUnlocked;
+
+}
