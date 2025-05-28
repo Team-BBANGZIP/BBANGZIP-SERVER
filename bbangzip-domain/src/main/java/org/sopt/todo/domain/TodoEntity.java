a60f43f39283923f127c8a66dfa9aec9e009a71c
yeeun0702 <kimyeeun0702@naver.com>
2025-05-28 18:13:08 +0900

    ✨ [feat] #12 날짜와 시간을 분리하여 LocalDate/LocalTime으로 변경

--- bbangzip-domain/src/main/java/org/sopt/todo/domain/TodoEntity.java
+++ bbangzip-domain/src/main/java/org/sopt/todo/domain/TodoEntity.java
@@ -1,14 +1,30 @@
 package org.sopt.todo.domain;
 
-import jakarta.persistence.*;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_CATEGORY_ID;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_CONTENT;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_ID;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_IS_COMPLETED;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_ORDER;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_START_TIME;
+import static org.sopt.todo.domain.TodoTableConstants.COLUMN_TARGET_DATE;
+import static org.sopt.todo.domain.TodoTableConstants.TABLE_TODO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.category.domain.CategoryEntity;
 import org.sopt.common.BaseTimeEntity;
-import java.time.LocalDateTime;
-
-import static org.sopt.todo.domain.TodoTableConstants.*;
 
 @Entity
 @Getter
@@ -29,13 +45,13 @@ public class TodoEntity extends BaseTimeEntity {
     private String content;
 
     @Column(name = COLUMN_START_TIME, nullable = false)
-    private LocalDateTime startTime;
+    private LocalTime startTime;
 
     @Column(name = COLUMN_IS_COMPLETED, nullable = false)
     private Boolean isCompleted;
 
     @Column(name = COLUMN_TARGET_DATE, nullable = false)
-    private LocalDateTime targetDate;
+    private LocalDate targetDate;
 
     @Column(name = COLUMN_ORDER, nullable = false)
     private int order;
