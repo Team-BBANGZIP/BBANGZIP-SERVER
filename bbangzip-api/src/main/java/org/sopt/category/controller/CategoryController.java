ef083429996510936e62a9c33b7c6c18671c6659
yeeun0702 <kimyeeun0702@naver.com>
2025-08-09 01:41:14 +0900

    ✨ [feat] #24 카테고리 조회 비즈니스 로직 구현

--- bbangzip-api/src/main/java/org/sopt/category/controller/CategoryController.java
+++ bbangzip-api/src/main/java/org/sopt/category/controller/CategoryController.java
@@ -4,10 +4,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.category.dto.request.CategoryCreateRequest;
 import org.sopt.category.dto.response.CategoryCreateResponse;
+import org.sopt.category.dto.response.CategoryResponse;
 import org.sopt.category.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,4 +34,15 @@ public class CategoryController {
                 .status(HttpStatus.CREATED)
                 .body(categoryService.createCategory(dummyUserId, categoryCreateRequest));
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories(
+            // TODO: 커스텀 어노테이션 final Long userId,
+    ) {
+        Long dummyUserId = 1L;
+        return ResponseEntity.ok(categoryService.getAllCategories(dummyUserId));
+    }
+
+
 }
