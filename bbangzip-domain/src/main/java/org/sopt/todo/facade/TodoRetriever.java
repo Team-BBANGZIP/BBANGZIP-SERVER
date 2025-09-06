1ccf88c546d91d3ba8eb19a6a1b3d723e36ac42c
yeeun0702 <kimyeeun0702@naver.com>
2025-09-07 00:43:11 +0900

    ✨ [feat] #34 투두 도메인 계층 조회 기능 구현

--- bbangzip-domain/src/main/java/org/sopt/todo/facade/TodoRetriever.java
+++ bbangzip-domain/src/main/java/org/sopt/todo/facade/TodoRetriever.java
@@ -1,18 +1,32 @@
 package org.sopt.todo.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.category.domain.CategoryEntity;
+import org.sopt.category.repository.CategoryRepository;
+import org.sopt.todo.domain.Todo;
+import org.sopt.todo.domain.TodoEntity;
 import org.sopt.todo.repository.TodoRepository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class TodoRetriever {
 
     private final TodoRepository todoRepository;
+    private final CategoryRepository categoryRepository;
 
     public int countByCategoryIdAndTargetDate(Long categoryId, LocalDate targetDate) {
         return todoRepository.countByCategoryIdAndTargetDate(categoryId, targetDate);
+    }
+
+    public List<Todo> findTodosByCategoryIdsAndDate(List<Long> categoryIds, LocalDate date) {
+        List<TodoEntity> entities = todoRepository.findByCategoryIdsAndDate(categoryIds, date);
+        return entities.stream()
+                .map(TodoEntity::toDomain)
+                .toList();
     }
 }
