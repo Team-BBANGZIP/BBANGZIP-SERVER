05135b0819ec6a09c7876c0f5dc9f8125b59eb10
choyeongju <jjudy1301@naver.com>
2025-09-14 18:51:30 +0900

    :sparkles: [feat] #39 JwtClaimsKeys

---
+++ bbangzip-auth/src/main/java/org/sopt/jwt/core/JwtClaimsKeys.java
@@ -0,0 +1,11 @@
+package org.sopt.jwt.core;
+
+public final class JwtClaimsKeys {
+    private JwtClaimsKeys() {}
+    public static final String ROLE      = "role";
+    public static final String PROVIDER  = "provider";
+    public static final String SESSIONID = "sid";
+    public static final String TYPE      = "type";
+    public static final String ACCESS    = "ACCESS_TOKEN";
+    public static final String REFRESH   = "REFRESH_TOKEN";
+}
