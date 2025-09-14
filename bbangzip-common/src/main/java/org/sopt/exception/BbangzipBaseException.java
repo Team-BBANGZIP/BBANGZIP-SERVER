f0027215ecccf9749cbec0c53bd887b4632fa931
yeeun0702 <kimyeeun0702@naver.com>
2025-09-14 20:05:44 +0900

    ✨ [feat] #36 BbangzipBaseException에 ErrorCode 필드 및 Getter 추가, 생성자 초기화 보장

--- bbangzip-common/src/main/java/org/sopt/exception/BbangzipBaseException.java
+++ bbangzip-common/src/main/java/org/sopt/exception/BbangzipBaseException.java
@@ -1,19 +1,36 @@
 package org.sopt.exception;
 
+import org.sopt.code.ErrorCode;
+
 public abstract class BbangzipBaseException extends RuntimeException {
-    public BbangzipBaseException() {
-        super();
+    private final ErrorCode errorCode;
+
+    public BbangzipBaseException(ErrorCode errorCode) {
+        super(errorCode != null ? errorCode.getMessage() : null);
+        this.errorCode = errorCode;
+    }
+
+    public BbangzipBaseException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
     }
 
     public BbangzipBaseException(String message) {
         super(message);
+        this.errorCode = null;
     }
 
     public BbangzipBaseException(String message, Throwable cause) {
         super(message, cause);
+        this.errorCode = null;
     }
 
     public BbangzipBaseException(Throwable cause) {
         super(cause);
+        this.errorCode = null;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
     }
 }
