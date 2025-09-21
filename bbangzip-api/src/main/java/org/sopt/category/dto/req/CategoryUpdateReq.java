3a258602aebec573d4b998600cd0c51fa6531a1b
yeeun0702 <kimyeeun0702@naver.com>
2025-09-21 14:15:16 +0900

    ✏️ [fix] #50 카테고리 update 시 공백/동일값 처리로 인한 NPE 및 제약조건 위반 수정

--- bbangzip-api/src/main/java/org/sopt/category/dto/req/CategoryUpdateReq.java
+++ bbangzip-api/src/main/java/org/sopt/category/dto/req/CategoryUpdateReq.java
@@ -1,11 +1,11 @@
 package org.sopt.category.dto.req;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.sopt.category.domain.CategoryColor;
 
 public record CategoryUpdateReq(
-        @NotBlank(message = "카테고리 이름은 필수입니다.")
+        @NotNull(message = "카테고리 이름은 필수입니다.")
         @Size(max = 25, message = "카테고리 이름은 25자 이내여야 합니다.")
         String name,
         CategoryColor color,
