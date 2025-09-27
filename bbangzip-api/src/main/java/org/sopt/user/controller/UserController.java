4a2186b933bbb74f59aa57b57121c34ce9ec5ffa
choyeongju <jjudy1301@naver.com>
2025-09-27 16:25:03 +0900

    :sparkles: [feat] #57 기본 정보 수정 API

--- bbangzip-api/src/main/java/org/sopt/user/controller/UserController.java
+++ bbangzip-api/src/main/java/org/sopt/user/controller/UserController.java
@@ -3,8 +3,11 @@ package org.sopt.user.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.jwt.annotation.UserId;
+import org.sopt.user.domain.User;
 import org.sopt.user.dto.req.CommitmentMessageCreateReq;
+import org.sopt.user.dto.req.UserProfileUpdateReq;
 import org.sopt.user.dto.res.CommitmentMessageRes;
+import org.sopt.user.dto.res.UserProfileUpdateRes;
 import org.sopt.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,4 +29,15 @@ public class UserController {
                 .status(HttpStatus.CREATED)
                 .body(userService.createCommitmentMessage(userId, commitmentMessageCreateReq));
     }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<UserProfileUpdateRes> updateProfile(
+            @UserId Long userId,
+            @Valid @RequestBody UserProfileUpdateReq request
+    ) {
+        User updatedProfile = userService.updateProfile(userId, request);
+        return ResponseEntity
+                .ok(UserProfileUpdateRes.from(updatedProfile));
+    }
+
 }
