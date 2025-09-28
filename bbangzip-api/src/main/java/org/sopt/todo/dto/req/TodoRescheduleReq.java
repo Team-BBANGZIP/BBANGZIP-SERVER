48e88899fb9ce985ad910db02cca481b94887b82
yeeun0702 <kimyeeun0702@naver.com>
2025-09-28 22:46:31 +0900

    ✨ [feat] #64 TodoRescheduleReq DTO 추가

---
+++ bbangzip-api/src/main/java/org/sopt/todo/dto/req/TodoRescheduleReq.java
@@ -0,0 +1,14 @@
+package org.sopt.todo.dto.req;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class TodoRescheduleReq {
+    @NotNull(message = "targetDate는 필수값입니다.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate targetDate;
+}
