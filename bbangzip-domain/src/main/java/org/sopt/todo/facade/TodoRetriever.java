46f9cd48a88d49aa2e4018476f78de5ba0604a1e
yeeun0702 <kimyeeun0702@naver.com>
2025-09-28 23:03:00 +0900

    ✨ [feat] #64 reschedule 비지니스 로직 구현(새 날짜 기준 orderIndex 계산 후 삽입)
    - 사용하지 않는 import문 제거

--- bbangzip-domain/src/main/java/org/sopt/todo/facade/TodoRetriever.java
+++ bbangzip-domain/src/main/java/org/sopt/todo/facade/TodoRetriever.java
@@ -1,16 +1,12 @@
 package org.sopt.todo.facade;
 
 import lombok.RequiredArgsConstructor;
-import org.sopt.category.domain.CategoryEntity;
-import org.sopt.category.repository.CategoryRepository;
 import org.sopt.todo.domain.Todo;
-import org.sopt.todo.domain.TodoEntity;
 import org.sopt.todo.domain.TodoEntity;
 import org.sopt.todo.repository.TodoRepository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
