b392dc12f93ae8eea1a6f35b8df6fe55b04f2019
choyeongju <jjudy1301@naver.com>
2025-10-02 01:34:12 +0900

    :sparkles: [feat] #62 유저별로 해금된 빵 조회하는 facade 로직

---
+++ bbangzip-domain/src/main/java/org/sopt/userbread/facade/UserBreadRetriever.java
@@ -0,0 +1,20 @@
+package org.sopt.userbread.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.userbread.domain.UserBreadEntity;
+import org.sopt.userbread.repository.UserBreadRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class UserBreadRetriever {
+
+    private final UserBreadRepository userBreadRepository;
+
+    public List<UserBreadEntity> findAllByUserId(Long userId) {
+        return userBreadRepository.findAllByUserId(userId);
+    }
+
+}
