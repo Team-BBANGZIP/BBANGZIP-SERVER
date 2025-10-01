cb67b8aef0ff1e53ca2644f1100031db3c50c3bf
choyeongju <jjudy1301@naver.com>
2025-10-02 01:33:53 +0900

    :sparkles: [feat] #62 유저별로 해금된 빵 조회

---
+++ bbangzip-domain/src/main/java/org/sopt/userbread/repository/UserBreadRepository.java
@@ -0,0 +1,19 @@
+package org.sopt.userbread.repository;
+
+import org.sopt.userbread.domain.UserBreadEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserBreadRepository extends JpaRepository<UserBreadEntity, Long> {
+
+    @Query("""
+        select distinct ub
+        from UserBreadEntity ub
+        join fetch ub.bread
+        where ub.user.id = :userId
+    """)
+    List<UserBreadEntity> findAllByUserId(@Param("userId") Long userId);
+}
