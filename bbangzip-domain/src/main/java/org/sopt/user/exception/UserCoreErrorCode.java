42014a096d216141ef004313b208204d5ec9d8f5
choyeongju <jjudy1301@naver.com>
2025-10-06 14:26:32 +0900

    :sparkles: [feat] #70 UserCoreErrorCode

--- bbangzip-domain/src/main/java/org/sopt/user/exception/UserCoreErrorCode.java
+++ bbangzip-domain/src/main/java/org/sopt/user/exception/UserCoreErrorCode.java
@@ -8,8 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UserCoreErrorCode implements ErrorCode {
-    INVALID_PROFILE_IMAGE_KEY(HttpStatus.BAD_REQUEST,40014, "잘못된 profileImageKey 입니다."),
 
+    // 400
+    INVALID_PROFILE_IMAGE_KEY(HttpStatus.BAD_REQUEST,40014, "잘못된 profileImageKey 입니다."),
+    INVALID_REGISTER_STATUS_TYPE(HttpStatus.BAD_REQUEST, 40415, "올바르지 않은 가입 타입입니다."),
+
+    // 404
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 40401, "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
