76318d9e96260a289aeb182525e5b1cc6b434793
yeeun0702 <kimyeeun0702@naver.com>
2025-10-07 03:55:31 +0900

    ✨ [feat] #79 날짜 변경 및 내일로 미루기 DTO 구현

--- bbangzip-api/src/main/java/org/sopt/todo/dto/req/TodoRescheduleReq.java
+++ bbangzip-api/src/main/java/org/sopt/todo/dto/req/TodoRescheduleReq.java
@@ -1,14 +1,9 @@
 package org.sopt.todo.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-
 import java.time.LocalDate;
 
-@Getter
-public class TodoRescheduleReq {
-    @NotNull(message = "targetDate는 필수값입니다.")
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate targetDate;
-}
+public record TodoRescheduleReq(
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate targetDate
+){}
