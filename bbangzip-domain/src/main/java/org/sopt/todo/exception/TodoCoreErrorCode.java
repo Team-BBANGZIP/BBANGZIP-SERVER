eaa1aefe46d6a5b3773b62851d2f46ef84ea62e4
yeeun0702 <kimyeeun0702@naver.com>
2025-10-09 00:03:35 +0900

    ✨ [feat] #82 투두 카테고리 및 순서 관련 에러 코드 추가

--- bbangzip-domain/src/main/java/org/sopt/todo/exception/TodoCoreErrorCode.java
+++ bbangzip-domain/src/main/java/org/sopt/todo/exception/TodoCoreErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum TodoCoreErrorCode implements ErrorCode {
 
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, 40406, "투두를 찾을 수 없습니다."),
-    INVALID_TODO_CONTENT(HttpStatus.BAD_REQUEST, 40013, "투두 내용은 0자일 수 없습니다.")
+    INVALID_TODO_CONTENT(HttpStatus.BAD_REQUEST, 40013, "투두 내용은 0자일 수 없습니다."),
+    TODO_CATEGORY_MISMATCH(HttpStatus.BAD_REQUEST, 40017, "요청한 카테고리가 실제 투두의 카테고리와 일치하지 않습니다."),
+    TODO_CATEGORY_COLOR_MISMATCH(HttpStatus.BAD_REQUEST, 40018, "요청한 카테고리 색상이 실제 카테고리와 일치하지 않습니다.");
     ;
 
     private final HttpStatus httpStatus;
