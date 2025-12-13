096e83aa2e678b26d7401de95ed94f687db79f8d
choyeongju <jjudy1301@naver.com>
2025-12-13 20:13:31 +0900

    :sparkles: [chore] #99 링크 오류 수정

--- bbangzip-domain/src/main/java/org/sopt/user/type/DefaultProfileImage.java
+++ bbangzip-domain/src/main/java/org/sopt/user/type/DefaultProfileImage.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public enum DefaultProfileImage {
     IMAGE_1(1, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/1.svg"),
-    IMAGE_2(2, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/docs/profile-images/2.svg"),
+    IMAGE_2(2, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/2.svg"),
     IMAGE_3(3, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/3.svg"),
     IMAGE_4(4, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/4.svg"),
     IMAGE_5(5, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/5.svg"),
