69f77d38ed9e9c51a15454daa917bddf4a7d27ff
choyeongju <jjudy1301@naver.com>
2025-12-13 20:13:02 +0900

    :sparkles: [chore] #99 링크 오류 수정

--- bbangzip-domain/src/main/java/org/sopt/user/type/DefaultProfileImage.java
+++ bbangzip-domain/src/main/java/org/sopt/user/type/DefaultProfileImage.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum DefaultProfileImage {
-    IMAGE_1(1, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/1.svg"),
-    IMAGE_2(2, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/2.svg"),
-    IMAGE_3(3, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/3.svg"),
-    IMAGE_4(4, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/4.svg"),
-    IMAGE_5(5, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/5.svg"),
-    IMAGE_6(6, "https://raw.githubusercontent.com/OWNER/REPO/develop/docs/profile-images/6.svg");
+    IMAGE_1(1, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/1.svg"),
+    IMAGE_2(2, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/docs/profile-images/2.svg"),
+    IMAGE_3(3, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/3.svg"),
+    IMAGE_4(4, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/4.svg"),
+    IMAGE_5(5, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/5.svg"),
+    IMAGE_6(6, "https://raw.githubusercontent.com/Team-BBANGZIP/BBANGZIP-SERVER/develop/docs/profile-images/6.svg");
 
     private final int key;
     private final String url;
