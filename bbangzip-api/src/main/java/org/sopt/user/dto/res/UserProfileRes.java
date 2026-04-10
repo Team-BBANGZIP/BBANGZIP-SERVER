0ea89f3779a16f6c9c7c18f53522bd4142f405da
yeeun0702 <kimyeeun0702@naver.com>
2026-04-11 05:43:49 +0900

    ✨ [feat] #109 프로필 조회 DTO에 profileImageKey 포함

--- bbangzip-api/src/main/java/org/sopt/user/dto/res/UserProfileRes.java
+++ bbangzip-api/src/main/java/org/sopt/user/dto/res/UserProfileRes.java
@@ -1,15 +1,18 @@
 package org.sopt.user.dto.res;
 
 import org.sopt.user.domain.User;
+import org.sopt.user.type.DefaultProfileImage;
 
 public record UserProfileRes(
         String profileImageUrl,
+        Integer profileImageKey,
         String nickname,
         String commitmentMessage
 ) {
     public static UserProfileRes from(User userProfile) {
         return new UserProfileRes(
                 userProfile.getProfileImage(),
+                DefaultProfileImage.findKeyByUrl(userProfile.getProfileImage()).orElse(null),
                 userProfile.getNickname(),
                 userProfile.getCommitmentMessage()
         );
